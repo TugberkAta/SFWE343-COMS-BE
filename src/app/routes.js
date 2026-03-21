91c0a12f023bc103c1305158a400d305464e2370
TugberkAta <atatugberkwork@gmail.com>
2026-03-21 19:51:19 +0200

    refactor: remove unused authorisation middleware and simplify user registration route

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const { ADMIN } = require("~root/constants/userRoles");
+// const { ADMIN } = require("~root/constants/userRoles");
 const postLogin = require("./controllers/users/login");
 const postUser = require("./controllers/users/register");
 const putUserDetails = require("./controllers/users/putUserDetails");
 const authentication = require("./middlewares/authentication");
-const authorise = require("./middlewares/authorisation");
+// const authorise = require("./middlewares/authorisation");
 const getUserRoles = require("./controllers/users/userRoles");
 const healthcheck = require("./platform/healthcheck");
 const postEmailAuth = require("./controllers/users/postEmailAuth");
@@ -15,12 +15,7 @@ const router = express.Router();
 // USER MANAGEMENT
 router.post("/login", postLogin);
 
-router.post(
-  "/register",
-  authentication,
-  authorise({ roles: [ADMIN] }),
-  postUser
-);
+router.post("/register", postUser);
 
 router.post("/email-auth", postEmailAuth);
 
