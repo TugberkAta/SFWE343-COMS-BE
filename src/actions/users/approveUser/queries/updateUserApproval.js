284b830b272fe7b29beec9bfc898265be523864d
“eylulkaratas” <eylulkaratas22@gmail.com>
2026-04-27 18:53:27 +0300

    feat: add userTypeId to approve user and update fetchUserById query

--- src/actions/users/approveUser/queries/updateUserApproval.js
+++ src/actions/users/approveUser/queries/updateUserApproval.js
@@ -3,11 +3,13 @@ const { submitQuery } = require("~root/lib/database");
 const updateUserApproval = ({
   userId,
   userRoleId,
+  userTypeId,
   approvedStatus
 }) => submitQuery`
   UPDATE users
   SET
     user_role_id = ${userRoleId},
+    user_type_id = ${userTypeId},
     approved = ${approvedStatus}
   WHERE user_id = ${userId}
 `;
--- src/actions/users/fetchUserById/queries/selectUserById.js
+++ src/actions/users/fetchUserById/queries/selectUserById.js
@@ -1,15 +1,19 @@
 const { submitQuery, camelKeys, getFirst } = require("~root/lib/database");
 
 const selectUserById = ({ userId }) => submitQuery`
-    SELECT 
-        user_id,
-        first_name,
-        last_name,
-        user_roles.user_role_id,
-        user_roles.user_role
-    FROM users
-    LEFT JOIN user_roles ON users.user_role_id = user_roles.user_role_id
-    WHERE user_id = ${userId}
+  SELECT
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    user_roles.user_role_id,
+    user_roles.user_role,
+    user_types.user_type_id,
+    user_types.type_name,
+    user_types.permissions_json
+  FROM users
+  LEFT JOIN user_roles ON users.user_role_id = user_roles.user_role_id
+  LEFT JOIN user_types ON users.user_type_id = user_types.user_type_id
+  WHERE user_id = ${userId}
 `;
 
 module.exports = getFirst(camelKeys(selectUserById));
